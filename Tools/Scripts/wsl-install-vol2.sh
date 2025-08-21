1530afaa25c5433eaae9bb6dacaac79b83a85fbc
N. Schmidt <n0raitor@pm.me>
2025-08-21 12:19:27 +0200

    Fix WSL Scripts

--- Tools/Scripts/wsl-install-vol2.sh
+++ Tools/Scripts/wsl-install-vol2.sh
@@ -9,12 +9,7 @@ python2 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipyt
 sudo python2 -m pip install yara
 sudo ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
 python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
-sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
 cd ~/.local/bin
 mv vol.py vol2.py
 cd ~
-git clone https://github.com/volatilityfoundation/volatility3.git
-cd volatility3/
-pip3.10 install -r requirements.txt
-echo "alias vol.py='python3 ~/volatility3/vol.py'" >> ~/.bashrc
 echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
