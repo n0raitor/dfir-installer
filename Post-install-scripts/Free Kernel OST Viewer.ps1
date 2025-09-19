424e7134379af71e3e9ca2f4b5acdaf4c063cb46
N. Schmidt <n0raitor@pm.me>
2025-09-19 14:54:22 +0200

    Add All Free Kernel Tools

--- Post-install-scripts/Free Kernel OST Viewer.ps1
+++ Post-install-scripts/Free Kernel OST Viewer.ps1
@@ -1,4 +1,4 @@
-# PowerShell PostProcess script for Free Kernel OST Viewer of Category C:\DFIR\_Tools\Email Tools
+# PowerShell PostProcess script for Kernel-OST-Viewer of Category C:\DFIR\_Tools\Email Tools
 
 # Check if the category path exists, if not, create it
 if (!(Test-Path -Path "C:\DFIR\_Tools\Email Tools")) {
@@ -6,15 +6,15 @@ if (!(Test-Path -Path "C:\DFIR\_Tools\Email Tools")) {
 }
 
 # Check if the tool directory exists
-if (Test-Path -Path "C:\DFIR\Free Kernel OST Viewer") {
+if (Test-Path -Path "C:\DFIR\Kernel-OST-Viewer") {
     # Create symlink if tool directory exists
-    New-Item -Path "C:\DFIR\_Tools\Email Tools\Free Kernel OST Viewer" -ItemType SymbolicLink -Target "C:\DFIR\Free Kernel OST Viewer" -Force
+    New-Item -Path "C:\DFIR\_Tools\Email Tools\Kernel-OST-Viewer" -ItemType SymbolicLink -Target "C:\DFIR\Kernel-OST-Viewer" -Force
 } else {
-    Write-Host "Tool directory does not exist: C:\DFIR\Free Kernel OST Viewer"
+    Write-Host "Tool directory does not exist: C:\DFIR\Kernel-OST-Viewer"
 }
 
 # Create symlink
-#New-Item -Path "C:\DFIR\_Tools\Email Tools\Free Kernel OST Viewer" -ItemType SymbolicLink -Target "C:\DFIR\Free Kernel OST Viewer\Free Kernel OST Viewer.exe" -Force
+#New-Item -Path "C:\DFIR\_Tools\Email Tools\Kernel-OST-Viewer" -ItemType SymbolicLink -Target "C:\DFIR\Kernel-OST-Viewer\Kernel-OST-Viewer.exe" -Force
 
 # File moving command will be inserted here
 # Move-Item -Path C:\DFIR\ -Destination "C:\DFIR\Tools\Email Tools"
