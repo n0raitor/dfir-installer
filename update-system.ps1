422b01957f0b83660c6705f96d62f527957705c1
N. Schmidt <n0raitor@pm.me>
2026-01-16 12:29:57 +0100

    Fix Update System Priority and Bugs

--- update-system.ps1
+++ update-system.ps1
@@ -1,12 +1,11 @@
 $folder = 'C:\DFIR\_dfir-installer'
 
 #winget update
-winget upgrade --all --include-unknown
-choco upgrade all -y
+Write-Host "Updating System Script is running..."
 
 ### Update Zimmermann
 # Pfad zur Datei definieren
-
+Write-Host "Starting Zimmerman Tools Update"
 $filePath = "C:\DFIR\Zimmerman\Get-ZimmermanTools.ps1"
 
 # Überprüfen, ob die Datei existiert
@@ -15,14 +14,8 @@ if (Test-Path $filePath) {
     Write-Host "$filePath existiert. Skript wird ausgeführt..."
     C:\DFIR\Zimmerman\Get-ZimmermanTools.ps1 -Dest C:\DFIR\Zimmerman
     #Move-Item -Path ".\net9" -Destination "C:\DFIR\Zimmerman\" -Force
-}
-
-# Überprüfen, ob die Datei existiert
-if (Test-Path $filePath) {
-    # Datei existiert, daher ausführen
-    Write-Host "$filePath existiert. Skript wird ausgeführt..."
-    C:\DFIR\KAPE\KAPE\kape.exe --sync
-    #Move-Item -Path ".\net9" -Destination "C:\DFIR\Zimmerman\" -Force
+} else {
+	Write-Host "$filePath does not exist. Skipping..."
 }
 
 $filePath = "C:\DFIR\KAPE\KAPE\Get-KAPEUpdate.ps1"
@@ -33,8 +26,31 @@ if (Test-Path $filePath) {
     Write-Host "$filePath existiert. Skript wird ausgeführt..."
     C:\DFIR\KAPE\KAPE\Get-KAPEUpdate.ps1
     #Move-Item -Path ".\net9" -Destination "C:\DFIR\Zimmerman\" -Force
+} else {
+	Write-Host "$filePath does not exist. Skipping..."
 }
 
+# Überprüfen, ob die Datei existiert
+if (Test-Path $filePath) {
+    # Datei existiert, daher ausführen
+    Write-Host "$filePath existiert. Skript wird ausgeführt..."
+    C:\DFIR\KAPE\KAPE\kape.exe --sync
+    #Move-Item -Path ".\net9" -Destination "C:\DFIR\Zimmerman\" -Force
+} else {
+	Write-Host "$filePath does not exist. Skipping..."
+}
+
+
+Write-Host "Starting: Winget Upgrade"
+winget update
+winget upgrade --all --include-unknown
+
+Write-Host "Starting: Chocolatey upgrade"
+choco upgrade all -y
+
+
+
+Write-Host "Starting: Export Configs"
 
 ##& chocolateygui.exe
 ##& "C:\Program Files\UniGetUI\UniGetUI.exe"
@@ -64,7 +80,7 @@ $exportFilechoco = Join-Path -Path $exportFolder -ChildPath "choco_export_$curre
 winget export -o $exportFile
 choco export $exportFilechoco
 
-Read-Host "Make sure to move unused Links that were dropped during the update to the Tools folder on your desktop. Press Enter to continue..."
+Read-Host "Make sure to move unused Links that were dropped during the update to the Tools folder on your desktop. Press Enter to continue with the DFIR-Installer Check"
 
 # Output the path where the file was saved
 Write-Host "Export completed. File saved to: $exportFile"
